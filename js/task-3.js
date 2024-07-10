da2e0af16bc7cbaf743aa6091f72b6737990cff3
RazanAl-Masri <166054247+RazanAl-Masri@users.noreply.github.com>
2024-07-10 16:17:45 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,6 @@ function getElementWidth(content, padding, border) {
     let paddingWidth = parseFloat(padding);
     let borderWidth = parseFloat(border);
     
-    // Calculate total width
     let totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
     
     return totalWidth;
@@ -13,4 +12,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
